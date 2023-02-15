37184c1409a00515bdfc7856d0109e2af2b86f03
Robell Gabriel <72244958+robellgabriel@users.noreply.github.com>
2023-02-15 13:27:45 -0500

    added templates and modify addressbook controller
    completed lab 6 part 1

--- src/main/java/com/example/Lab5/AddressBookControllerTL.java
+++ src/main/java/com/example/Lab5/AddressBookControllerTL.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class AddressBookControllerTL {
     @Autowired
@@ -14,10 +16,42 @@ public class AddressBookControllerTL {
         this.addressBookRepository = addressBookRepository;
     }
 
-    @RequestMapping("/viewAddressBook")
-    public String viewAddressBook(@RequestParam("id") int id, Model model){
-        AddressBook addressBook = addressBookRepository.findByID(id);
-        model.addAttribute("addressBook", addressBook);
-        return "index";
+    @GetMapping("/addAddressBook")
+    public String viewAddressBook(){
+        return "addAddressBook";
     }
+
+    @PostMapping("/viewAddressBook")
+    public String buildAddressBook(@RequestParam("id") int id, Model model){
+        AddressBook addressBook = new AddressBook();
+        addressBook.setID(id);
+        addressBookRepository.save(addressBook);
+        List<AddressBook> listAB = (List<AddressBook>) addressBookRepository.findAll();
+        model.addAttribute("listAB", listAB);
+        return "viewAddressBook";
+    }
+
+    @GetMapping("/addBuddyInfoView")
+    public String viewBuddyInfo(){
+        return "addBuddy";
+    }
+
+    @PostMapping("/viewBuddyInfo")
+    public String addBuddyInfoView(@RequestParam("addressId") int addressId,
+                               @RequestParam("buddyId") int buddyId,
+                               @RequestParam("name") String name,
+                               @RequestParam("phoneNum") String phoneNum,
+                                   Model model){
+        BuddyInfo buddyInfo = new BuddyInfo();
+        buddyInfo.setID(buddyId);
+        buddyInfo.setName(name);
+        buddyInfo.setPhoneNumber(phoneNum);
+        AddressBook addressBook = addressBookRepository.findByID(addressId);
+        addressBook.addBuddy(buddyInfo);
+        addressBookRepository.save(addressBook);
+        model.addAttribute("addressBook", addressBook);
+        return "viewBuddyInfo";
+    }
+
+
 }
